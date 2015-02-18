b91f1382bfb29bd2075ffe403b30138a9a22dda0
Justin Schaefer <jds_06@hotmail.com>
2015-02-18 12:31:40 -0600

    Finished constructor

--- ListArray.cpp
+++ ListArray.cpp
@@ -78,5 +78,92 @@ void List<DataType>::clear(){
 		size = 0;
 		cursor = 0;
 	}
-
 }
+
+template < typename DataType>
+bool List<DataType>::isEmpty() const{  //Member not found???
+	if(size > 0){
+		return false;
+	} else {
+		return true;
+	}
+}
+
+template < typename DataType>
+bool List<DataType>::isFull() const{  //Member not found???
+	if(size >= maxSize){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+template < typename DataType>
+void List<DataType>::gotoBeginning() throw (logic_error){
+	if(cursor != 0){
+		cursor = 0;
+	} else {
+		throw logic_error("Already at the beginning.");
+	}
+}
+
+template < typename DataType>
+void List<DataType>::gotoEnd() throw (logic_error){
+	if(cursor != maxSize){
+		cursor = maxSize;
+	} else {
+		throw logic_error("Already at the end.");
+	}
+}
+
+template < typename DataType>
+bool List<DataType>::gotoNext() throw (logic_error){
+	if(cursor == maxSize){
+		throw logic_error("Cannot go to next, already at the end.");
+		return false;
+	} else {
+		cursor++;
+		return true;
+	}
+}
+
+template < typename DataType>
+bool List<DataType>::gotoPrior() throw (logic_error){
+	if(cursor == maxSize){
+		throw logic_error("Cannot go back, already at the beginning.");
+		return false;
+	} else {
+		cursor--;
+		return true;
+	}
+}
+
+template < typename DataType>
+DataType List<DataType>::getCursor() const throw(logic_error){
+	if(dataItems[cursor] == 0){
+		throw ("There is nothing to retrieve.");
+	} else {
+		return dataItems[cursor];
+	}
+}
+
+template < typename DataType>
+void List<DataType>::showStructure () const{
+	cout << "Max size: " << maxSize << endl;
+	cout << "Actual size: " << size << endl;
+	cout << "Cursor position: " << cursor << endl;
+	cout << "Data: " << endl;
+
+	for(int x = 0; 0 <= size; x++){
+		cout << dataItems[x] << endl;
+	}
+}
+
+//template < typename DataType>
+//void moveToNth(int n) throw (logic_error){
+//	if(n > maxSize || n < 0){
+//		throw logic_error("Out of bounds.");
+//	} else {
+//
+//	}
+//}
